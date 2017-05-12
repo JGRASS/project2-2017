4386df2160447eb47fe8e1bbff4ec1d8c93e04ed
Miljan Ignjatovic <miljan.ignjatovic.96@gmail.com>
2017-05-12 14:14:09 +0200

    Dodata javadoc za klasu DBConnector.

--- GymIS/src/connection/DBConnector.java
+++ GymIS/src/connection/DBConnector.java
@@ -1,15 +1,27 @@
 package connection;
 
 import java.sql.*;
-
+/**
+ * Klasa koja omogucava konekciju sa relacionom bazom podataka.
+ * 
+ * @author Miljn Ignjatovic
+ * @version 1.0
+ */
 public class DBConnector {
-
+	/**
+	 * Atribut predstaljva samu konekciju sa bazom.
+	 */
 	Connection con = null;
 
 	String url = "jdbc:mysql://localhost:3306/GymIS";
 	String user = "GymWorker";
 	String pass = "pass123";
 
+	/**
+	 * Metoda vraca konekciju sa bazom.
+	 * @return con 
+	 * @throws java.sql.SQLException
+	 */
 	public Connection connect() throws SQLException {
 		con = DriverManager.getConnection(url, user, pass);
 		return con;
